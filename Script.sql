9d69fbe28f425a4279e3b0598a7dd9ef2213d50c
John Bolorinos <jkbolorinos@gmail.com>
2023-05-30 08:45:08 +0200

    First commit
    Adding database data

---
+++ Script.sql
@@ -0,0 +1,6 @@
+INSERT INTO STUDENTS VALUES('Abdelkader',24,'Canada'),('Almira',27,'France'),('Ankit',30,'USA'),('Baiyr',31,'India'),('Benjamin',29,'France'),('Bethany',24,'USA'),('Daniel',24,'USA'),('David',25,'Canada'),('Deepa',32,'India'),('Filippo',25,'Germany'),('Florian',30,'Germany'),('Fred',25,'USA'),('Gregory',27,'Germany'),('Harish',32,'Spain'),('Heinrich',26,'France'),('Hugo',22,'France'),('Jaime',26,'Spain'),('Jordan',24,'Italy'),('Jose',32,'Spain'),('Latika',22,'India'),('Lucia',28,'Spain'),('Luisa',22,'Spain'),('Matias',26,'USA'),('Maurice',28,'Italy'),('Mohammad',28,'Canada'),('Nicola',29,'USA'),('Patricio',22,'Germany'),('Peian',31,'India'),('Prasoon',26,'Germany'),('Samuel',23,'Canada'),('Sebastianus',25,'Germany'),('Shashank',27,'Italy'),('Siddharth',23,'India'),('Sondos',23,'France'),('Stefano',29,'Canada'),('Tanmoy',27,'Germany'),('Tetiana',23,'Italy'),('Teunis',31,'Canada'),('Theodore',30,'Italy');
+
+SELECT * FROM STUDENTS;
+
+
+
